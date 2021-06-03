aa68f7dad2fb5c6ecff047381279f150e9c25ecc
Gaukhar Kashaganova <gaukharka@personal-mac-gaukhar.home>
2021-06-03 16:27:03 +0200

    adds components and validation

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import MainPage from '../main-page/main-page.jsx';
+import PropTypes from 'prop-types';
+function App(props) {
+  const {title, genre, releaseDate} = props;
 
-function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <MainPage
+      title={title}
+      genre={genre}
+      releaseDate={releaseDate}
+    />
+  );
 }
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  releaseDate: PropTypes.number.isRequired,
+};
 
 export default App;
